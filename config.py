78cffc27867da18dbf9a3a4714297a7597096cf9
Priscilla <kubipriscilla0@gmail.com>
2024-09-24 14:19:48 +0000

    Resolved conflict in config.py

--- config.py
+++ config.py
@@ -1,5 +1,8 @@
+
+import os
+
 class Config:
     SECRET_KEY = os.getenv('SECRET_KEY')
-    SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL') 
+    SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-    WEATHER_API_KEY = os.getenv('WEATHER_API_KEY') 
+    WEATHER_API_KEY = os.getenv('WEATHER_API_KEY')
